45914459ee322f202dc318f35d869e67cdd49d34
batsuev <batsuev@gmail.com>
2015-09-17 10:51:55 +0800

    Initial Commit

---
+++ AnyChartObjCSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnyChartObjCSample
+//
+//  Created by Alexandr Batcuyev on 9/17/15.
+//  Copyright (c) 2015 AnyChart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnyChartObjCSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnyChartObjCSample
+//
+//  Created by Alexandr Batcuyev on 9/17/15.
+//  Copyright (c) 2015 AnyChart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
